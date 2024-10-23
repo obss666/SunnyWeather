ce2130b201eaa0c46f5cb3d7004e46a0b6c7343a
ByteDance <313312371@qq.com>
2024-10-23 14:54:11 +0800

    实现搜索全球城市数据功能

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,10 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -45,4 +49,12 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("com.google.android.material:material:1.1.1")
+    implementation("com.google.code.gson:gson:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.6.1")
+    implementation("com.squareup.retrofit2:converter-gson:2.6.1")
+
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.2.0")
+
 }
---
+++ app/src/main/java/com/sunnyweather/android/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.sunnyweather.android
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.sunnyweather.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
